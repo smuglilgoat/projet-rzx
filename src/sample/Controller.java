f4970a6b33c26c499b684718ef0ab3991d16466a
Xeno <mohamedanis.bouaza@etu.usthb.dz>
2020-06-30 22:41:57 +0100

    XDDDDDD

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,9 +3,10 @@ package sample;
 public class Controller {
     public javafx.scene.control.TextField textField1;
     public javafx.scene.control.TextField textField2;
-
+    public javafx.scene.control.TextField XD3;
     public void buttonClicked() {
         System.out.println(textField1.getText());
         System.out.println(textField2.getText());
+        System.out.println(XD3.getText());
     }
 }
