f6ebafec0254fb341394df56217bc620dae2899f
Xeno <mohamedanis.bouaza@etu.usthb.dz>
2020-07-02 22:32:50 +0100

    login finale

--- src/Login.java
+++ src/Login.java
@@ -1,10 +1,20 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 
 public class Login extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    public void closeWindow(){
+        System.exit(0);
+    }
 
     public static void main(String[] args) {
         launch(args);
@@ -15,6 +25,26 @@ public class Login extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        //grab your root here
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+
+        //move around here
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
         primaryStage.show();
 
     }
